96c2b7f148783f869754e8016fcc376b6dfaea56
Shawn Sandy <creative@shawnsandy.design>
2021-10-11 19:11:47 -0400

    feat(wip): test webpack/vite shared components
    - vite does not work well with shared components at this time

--- react-components/src/components/index.js
+++ react-components/src/components/index.js
@@ -1,3 +1,13 @@
+/**
+ * Setup
+ * install packages: npm i --save-dev babel-cli babel-preset-env babel-preset-react babel-preset-stage-2
+ * create babel.config.js in package root
+ * copy and past this index.js file into you components folder
+ * add the following command to package.json
+ * "compile": "cross-env NODE_ENV=production npx babel src/components --out-dir dist --copy-files --watch"
+ * create the component and import into index.js see example below
+ * run the command in package.json `npm run compile`
+ */
 import Button from './Buttons/Button'
 
 export { Button }
